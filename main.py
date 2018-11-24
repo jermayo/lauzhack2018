6cf6094374ce4d1e39af63b5565b6de88e4848e9
jermayo <jeremy.mayoraz@gmail.com>
2018-11-24 14:29:54 +0100

    changed main

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-
+import time
 import pygame
 
 
 
 pygame.init()
+
+larg=800
+haut=500
+main=pygame.display.set_mode([larg,haut])
+
+while True:
+    time.sleep(0.5)
+    main.fill((0,0,0))
+    pygame.display.flip()
+    time.sleep(0.5)
+    main.fill((255,0,0))
+
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            break
+    pygame.display.flip()
+
+
+pygame.quit()
