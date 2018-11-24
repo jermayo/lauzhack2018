47cdef8d606879bfb54536d155b3751401805b19
Jérémy Mayoraz <42137167+jermayo@users.noreply.github.com>
2018-11-24 16:11:20 +0100

    Delete player.py

--- player.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import math
-
-class player():
-    def __init__(self, main):
-        return
-
-    def image(self, main):
-        pygame.draw.polygon(main, (0,0,0), [[100,100], [200,200], [200,100]], 2)
