3f0407db72d2c08b5c3eb77d2411b31fe0aa023e
OlinB <45310038+OlinB@users.noreply.github.com>
2018-11-24 21:18:23 +0100

    turret full canon control
    Hello There

--- turret.py
+++ turret.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import utilitary
 
 class turret():
 
@@ -15,12 +16,27 @@ class turret():
 
         pygame.draw.polygon(main, (0,0,0), [[self.x + self.size, self.y], [self.x - self.size, self.y], [self.x - int(self.size / 1.8), self.y - self.size], [self.x + int(self.size / 1.8), self.y - self.size]], 1)
         pygame.draw.arc(main, (0,0,0), [self.x - int(self.size / 2), self.y - self.size*3/2, self.size, self.size], 0, math.pi)
-        if(playerY < self.y + self.size):
-            angle = math.atan((self.y-playerY) / (playerX - self.x))
-            baseX = self.x + int(math.cos(angle) * self.size)
-            baseY = self.y - self.size + int(math.sin(angle) * self.size)
-            pygame.draw.line(main, (0,0,0), [baseX, baseY], [baseX + int(math.cos(angle) * self.size), baseY - int(math.sin(angle) * self.size)], 5)
-        #else if(playerX > self.x):
-        #    pygame.draw.line(main, (0,0,0), )
+        if(playerX == self.x):
+            self.angle_cannon = math.pi / 2
+        elif(playerX < self.x):
+            self.angle_cannon = math.pi - math.atan((self.y-playerY) / (self.x - playerX))
         else:
-            pygame.draw.line(main, (0,0,0), [self.x - int(self.size / 2), self.y - self.size], [self.x - self.size, self.y - self.size], 5)
+            self.angle_cannon = math.atan((self.y-playerY) / (playerX - self.x))
+
+        if(playerY < self.y - self.size):
+            baseX = int(self.x + math.cos(self.angle_cannon) * self.size / 2)
+            baseY = int(self.y - self.size - math.sin(self.angle_cannon) * self.size / 2)
+            pygame.draw.line(main, (0,0,0), [baseX, baseY], [baseX + int(math.cos(self.angle_cannon) * self.size /2), baseY - int(math.sin(self.angle_cannon) * self.size / 2)], 5)
+        else:
+            if(playerX < self.x):
+                pygame.draw.line(main, (0,0,0), [self.x - int(self.size / 2), self.y - self.size], [self.x - self.size, self.y - self.size], 5)
+            else:
+                pygame.draw.line(main, (0,0,0), [self.x + int(self.size / 2), self.y - self.size], [self.x + self.size, self.y - self.size], 5)
+
+
+    def getCoord(self):
+        angle1 = utilitary.coord(self.x + self.size, self.y)
+        angle2 = utilitary.coord(self.x - self.size, self.y)
+        sommet = utilitary.coord(self.x, int(self.y - 3 / 2 * self.size))
+
+        return [angle1, angle2, sommet]
