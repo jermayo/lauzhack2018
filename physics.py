a92cd9f01c7f9cb0ee12fbd8625dd6b16c2fff84
jermayo <jeremy.mayoraz@gmail.com>
2018-11-24 20:12:39 +0100

    physic world

--- physics.py
+++ physics.py
@@ -19,14 +19,14 @@ class physical_point():
         new_coord={}
         for i in ["x","y"]:
             new_coord[i]=self.accel[i]*t*t/2+self.speed[i]*t+self.coord[i]
-#        for obstacle in obstacle_list:
-#            if collision(obstacle.coord, )
+        for obstacle in obstacle_list:
+            if collision(obstacle.coord, )
 
     def draw(self, object_list):
         self.update(object_list)
 
 def collision(hb1, hb2):
-    #[[min_x, min_y],[min_y,max_y]]
+    #[[min_x, max_x],[min_y,max_y]]
     def get_hit_box(hb):
         l=[]
         for coord in ["x","y"]:
@@ -36,22 +36,31 @@ def collision(hb1, hb2):
             l.append([min(l2),max(l2)])
         return l
 
+    def point_inside(p, x, y):
+        if p[0]>x[0] and p[0]<x[1] and p[1]>y[0] and p[1]<y[1]:
+            print(p,x,y)
+            return True
+
+        return False
+
+    len1=len(hb1)
+    len2=len(hb2)
     hb1=get_hit_box(hb1)
     hb2=get_hit_box(hb2)
 
     print(hb1, hb2)
+    if len1==1 and len2==1:
+        return False
+    elif len1==1:
+        return point_inside([hb1[0][0],hb1[1][0]], hb2[0], hb2[1])
+    elif len2==1:
+        return point_inside([hb2[0][0],hb2[1][0]], hb1[0], hb1[1])
 
+    flag=False
     for i in range(2):
         for j in range(2):
-            if hb1[i][j]>hb2[i][0] and hb1[i][j]<hb2[i][1]:
+            if point_inside([hb1[0][i],hb1[1][j]], hb2[0], hb2[1]) or point_inside([hb2[0][i],hb2[1][j]], hb1[0], hb1[1]):
                 return True
-
     return False
 
-
-
-    min_y=min([i for i in hb1["x"]])
-    max_y=max([i for i in hb1["x"]])
-
-    return False
 g=9.81
