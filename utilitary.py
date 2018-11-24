20def8641599c26650509eb0be6fba016a2bc232
jermayo <jeremy.mayoraz@gmail.com>
2018-11-24 17:02:29 +0100

    utilitary

---
+++ utilitary.py
@@ -0,0 +1,18 @@
+import math
+
+#returns cartesian coord from polar
+def cart_coord(radius, theta):
+    return radius*math.cos(theta), radius*math.sin(theta)
+
+#returns polar coord from cartesian
+def polar_coord(x, y):
+    if x==0:
+        if y>0:
+            angle=math.pi/2
+        elif y<0:
+            angle=-math.pi/2
+        elif y==0:
+            angle=0
+    else:
+        angle=math.atan(y/x)
+    return math.sqrt(x*x+y*y), angle
