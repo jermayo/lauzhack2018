96e180fb9c948f972d42d457422b7e08823c0081
jermayo <jeremy.mayoraz@gmail.com>
2018-11-24 22:10:30 +0100

    fullscreen working

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ import utilitary
 GV=utilitary.GlobalVariable()
 pygame.init()
 
-larg=800
-haut=500
 
+infoObject = pygame.display.Info()
+larg, haut=infoObject.current_w, infoObject.current_h
+
+infoObject.current_w, infoObject.current_h
 main=pygame.display.set_mode([larg,haut])
-pygame.display.toggle_fullscreen()
+fullscreen=False
+
 
 horloge=pygame.time.Clock()
 
@@ -22,21 +25,26 @@ obj_list=[]
 run=True
 while run:
     main.fill((255,255,255))
-    obj.rect(0, 0, 200, 200, GV).draw(main)
 
 
-    #player1.image(main)
-    #[head1, head2, head3, arm1, arm2, foot1, foot2] = player1.getCoord()
-    #pygame.draw.polygon(main, (0,0,0), [[head1["x"], head1["y"]], [head2["x"], head2["y"]], [arm1["x"], arm1["y"]], [foot1["x"], foot1["y"]], [foot2["x"], foot2["y"]], [arm2["x"], arm2["y"]], [head3["x"], head3["y"]]], 1)
-    #player1.run(t)
+
+    player1.image(main)
+    [head1, head2, head3, arm1, arm2, foot1, foot2] = player1.getCoord()
+    pygame.draw.polygon(main, (0,0,0), [[head1["x"], head1["y"]], [head2["x"], head2["y"]], [arm1["x"], arm1["y"]], [foot1["x"], foot1["y"]], [foot2["x"], foot2["y"]], [arm2["x"], arm2["y"]], [head3["x"], head3["y"]]], 1)
+    player1.run(t)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
 
     touche=pygame.key.get_pressed()
+    if touche[pygame.K_F11]:
+        if not fullscreen:
+            pygame.display.set_mode([larg,haut], pygame.FULLSCREEN)
+            fullscreen=not fullscreen
     if touche[pygame.K_ESCAPE]:
-        pygame.display.toggle_fullscreen()
-
+        if fullscreen:
+            pygame.display.set_mode([larg,haut])
+            fullscreen=not fullscreen
     pygame.display.flip()
     horloge.tick(60)
     t += 0.2
