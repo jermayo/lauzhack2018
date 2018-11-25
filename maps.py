4c071f2e044713d8629733bae7fe92eda964b58a
Loïc Cordey <loic.cordey@gmail.com>
2018-11-25 09:33:09 +0100

    hiiii

--- maps.py
+++ maps.py
@@ -46,3 +46,49 @@ def setmap2(GV,main,s,b, player):
     obj.turret(9*s,b-5*s,GV).image(main, player=player, GV=GV)
 
     obj.flag(23*s,b-5*s,s,GV).image(main, player=player, GV=GV)
+
+def setmaptr(GV,main,s,b, player,t):
+
+
+    obj.rect(0-t*25*s/500, b, 25*s-t*25*s/500, b-1*s, GV).image(main)
+    obj.rect(5*s-t*25*s/500, b, 7*s-t*25*s/500, b-2*s, GV).image(main)
+    obj.rect(6*s-t*25*s/500, b, 9*s-t*25*s/500, b-3*s, GV).image(main)
+    obj.rect(11*s-t*25*s/500, b, 14*s-t*25*s/500, b-3*s, GV).image(main)
+    obj.rect(15*s-t*25*s/500, b-4*s, 16*s-t*25*s/500, b-5*s, GV).image(main)
+    obj.rect(17*s-t*25*s/500, b-5*s, 19*s-t*25*s/500, b-6*s, GV).image(main)
+    obj.rect(21*s-t*25*s/500, b, 25*s-t*25*s/500, b-6*s, GV).image(main)
+    obj.rect(0-t*25*s/500, b-11*s, 25*s-t*25*s/500, 0, GV).image(main)
+    obj.rect(0-t*25*s/500, 0, 1*s-t*25*s/500, b, GV).image(main)
+
+    obj.flag(23*s-t*25*s/500,b-6*s,s,GV).image(main, player=player, GV=GV)
+
+    obj.spikes(9*s-t*25*s/500,b-1*s,2,s,GV).image(main, player=player, GV=GV)
+    obj.spikes(14*s-t*25*s/500,b-1*s,7,s,GV).image(main,player=player, GV=GV)
+
+    obj.excla(4*s-t*25*s/500,b-1*s,GV).image(main, GV=GV)
+
+
+
+    
+    obj.rect(0+(500-t)*25*s/500, 0, 25*s+(500-t)*25*s/500, b-11*s, GV).image(main)
+    obj.rect(0+(500-t)*25*s/500, b, 25*s+(500-t)*25*s/500, b-1*s-1, GV).image(main)
+
+    obj.rect(0+(500-t)*25*s/500, b-6*s, 2*s+(500-t)*25*s/500, b-1*s, GV).image(main)
+    obj.rect(2*s+(500-t)*25*s/500, b-5*s, 3*s+1+(500-t)*25*s/500, b-1*s, GV).image(main)
+    obj.rect(3*s+(500-t)*25*s/500, b-1*s, 4*s+1+(500-t)*25*s/500, b-4*s, GV).image(main)
+    obj.rect(4*s+(500-t)*25*s/500, b-1*s, 5*s+1+(500-t)*25*s/500, b-3*s, GV).image(main)
+    obj.rect(5*s+(500-t)*25*s/500, b-1*s, 6*s+1+(500-t)*25*s/500, b-2*s, GV).image(main)
+    obj.rect(14*s+(500-t)*25*s/500, b-2*s-1, 25*s+1+(500-t)*25*s/500, b-1*s, GV).image(main)
+    obj.rect(15*s+(500-t)*25*s/500, b-3*s-1, 25*s+1+(500-t)*25*s/500, b-2*s, GV).image(main)
+    obj.rect(16*s+(500-t)*25*s/500, b-4*s-1, 25*s+1+(500-t)*25*s/500, b-3*s, GV).image(main)
+    obj.rect(22*s+(500-t)*25*s/500, b-5*s-1, 25*s+1+(500-t)*25*s/500, b-4*s, GV).image(main)
+
+    obj.rect(8*s+(500-t)*25*s/500, b-5*s, 12*s+(500-t)*25*s/500, b-4*s, GV).image(main)
+    obj.rect(10*s+(500-t)*25*s/500, b-5*s, 12*s+(500-t)*25*s/500, b-11*s, GV).image(main)
+    obj.rect(12*s+(500-t)*25*s/500, b-6*s, 19*s+(500-t)*25*s/500, b-11*s, GV).image(main)
+
+    obj.excla(11*s+(500-t)*25*s/500,b-1*s,GV).image(main, GV=GV)
+    obj.clock(9*s+(500-t)*25*s/500,b-1*s,s,GV).image(main)
+    obj.turret(9*s+(500-t)*25*s/500,b-5*s,GV).image(main, player=player, GV=GV)
+
+    obj.flag(23*s+(500-t)*25*s/500,b-5*s,s,GV).image(main, player=player, GV=GV)
