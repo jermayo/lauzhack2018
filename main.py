1279ab6333231b4d44fbbfbc47bd8ebac6aaf357
OlinB <45310038+OlinB@users.noreply.github.com>
2018-11-25 03:54:33 +0100

    minor fix

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ def key_action(keys, fullscreen, player1, time):
     if keys[pygame.K_d]:
         player1.running=True
         player1.state(time, side="RIGHT")
-        player1.elem.center.speed=utilitary.coord(5,0)
+        player1.elem.center.speed["x"]=5
     elif keys[pygame.K_a]:
         player1.running=True
         player1.state(time, side="LEFT")
-        player1.elem.center.speed=utilitary.coord(-5,0)
+        player1.elem.center.speed["x"]=-5
     else:
         player1.running=False
         player1.state(time)
