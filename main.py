9702231a51bc4b9013777d84b6246432f1513f29
jermayo <jeremy.mayoraz@gmail.com>
2018-11-25 08:20:59 +0100

    main

--- main.py
+++ main.py
@@ -44,8 +44,6 @@ def key_action(keys, fullscreen, player1, time):
         if GV.timeSpeed>=-1:
             GV.timeSpeed-=0.1
 
-
-
     return fullscreen
 
 GV=utilitary.GlobalVariable()
@@ -79,21 +77,21 @@ next_level=0
 
 while mainrun and GV.level_pass<2:
 
-    if GV.level_pass==1 and next_level==0:
-        main.fill((255,255,255))
-        
-    
-        next_level=1
-    if next_level:
+    #if GV.level_pass==1:
+    #    main.fill((255,255,255))
+
+
+
+    if GV.level_pass==1:
         l=level_pass
         GV=utilitary.GlobalVariable()
-        GV.level_pass=1
+        GV.level_pass=l
         GV.size=larg/maxMapSize
 
         player1=player.mister(larg/8, haut/2, GV)
         player1.elem.points_list=player1.getCoord()
-
         maps.setmap2(GV, main, GV.size, haut, player1)
+        
     main.fill((255,255,255))
     #GV.obj_list=[]
     fullscreen=key_action(pygame.key.get_pressed(), fullscreen, player1, t)
